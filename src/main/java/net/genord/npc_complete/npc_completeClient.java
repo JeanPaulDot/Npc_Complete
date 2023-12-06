bfea9ac9739013dbb470cd33cb9461f6bf0cb84d
JeanPaulDOT <jp.houssier@gmail.com>
2023-12-06 12:00:53 +0100

    Initial commit

---
+++ src/main/java/net/genord/npc_complete/npc_completeClient.java
@@ -0,0 +1,11 @@
+package net.genord.npc_complete;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class npc_completeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
